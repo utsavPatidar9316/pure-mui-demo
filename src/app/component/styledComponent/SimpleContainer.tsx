9e3ea07dbbe491f936b9de60d8f797f56b86f3bb
ParthP-netpair <parth.parmar@netpairinfotech.com>
2024-04-26 09:39:16 +0530

    commit

--- src/app/component/styledComponent/SimpleContainer.tsx
+++ src/app/component/styledComponent/SimpleContainer.tsx
@@ -2,27 +2,28 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import Image from "next/image";
 
-const dummyData = [
-  { id: 1, text: 'Box 1' },
-  { id: 2, text: 'Box 2' },
-  { id: 3, text: 'Box 3' },
-  { id: 4, text: 'Box 4' }
-];
+import { container_data } from '@/app/utils/data';
+
+
 
 export default function SimpleContainer() {
   return (
     <React.Fragment>
 
 
-      <Box sx={{ bgcolor: '#624bff', height: '160px', width: '100%', padding: "0px" }} >
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', margin: "auto", padding: "40px 20px 0px 20px" }}>
-          <Box>Project</Box>
+      <Box sx={{ bgcolor: '#624bff', height: '160px', width: '100%', padding: '0px' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', margin: 'auto', padding: '40px 20px 0px 20px', maxWidth: '1200px', alignItems: 'center' }}>
+          <Box sx={{ fontSize: '24px', color: 'white' }}>Project</Box>
           <Button variant="contained" size="large" sx={{
-            bgcolor: 'white', color: 'black', '&:hover': {
-              bgcolor: 'white', color: 'black'
+            bgcolor: 'white',
+            color: 'black',
+            '&:hover': {
+              bgcolor: 'white',
+              color: 'black'
             }
-          }} >
+          }}>
             Create New Project
           </Button>
         </Box>
@@ -30,13 +31,30 @@ export default function SimpleContainer() {
 
       <Box sx={{ position: 'relative', top: -30, left: 0, margin: "10px" }}>
         <Grid container spacing={2}>
-          {dummyData.map(item => (
-            <Grid key={item.id} item xs={12} sm={6} md={4} lg={3}>
+          {container_data.map((item: any, key: any) => (
+            <Grid key={key.id} item xs={12} sm={6} md={6} lg={3}>
+
               <Box sx={{
-                bgcolor: 'white', height: 100, color: 'black', borderRadius: 2,
-                boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',textAlign:"center",lineHeight:"100px"
+                bgcolor: 'white',  color: 'black', borderRadius: 2,
+                boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+
               }}>
-                {item.text}
+                <Box sx={{display:'flex',justifyContent:"space-between"}}>
+                  <Box padding={2}>
+                    <Box >
+                      {item.title}
+                    </Box>
+                    <Box>
+                      {item.number}
+                    </Box>
+                    <Box>
+                      {item.completed}
+                    </Box>
+                  </Box>
+                  <Box >
+                  <Image src={item.src} alt={item.title} width={150} height={150} />
+                  </Box>
+                </Box>
               </Box>
             </Grid>
           ))}
---
+++ src/app/utils/data.ts
@@ -0,0 +1,26 @@
+export const container_data = [
+    {
+      title: "Projects",
+      number: "18",
+      completed: "2 completed",
+      src: "projectIcon.svg",
+    },
+    {
+      title: "Active Task",
+      number: "132",
+      completed: "28 completed",
+      src: "activeTask.svg",
+    },
+    {
+      title: "Teams",
+      number: "12",
+      completed: "1 completed",
+      src: "termsIcon.svg",
+    },
+    {
+      title: "Productivity",
+      number: "76%",
+      completed: "5% completed",
+      src: "productivityIcon.svg",
+    },
+  ];
